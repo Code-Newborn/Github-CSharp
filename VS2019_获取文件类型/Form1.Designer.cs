e9a9cc05e1e80d62ff3d89d6e7185be656001ddf
CZ <1253709179@qq.com>
2020-12-08 11:00:59 +0800

    VS2019_获取文件类型

---
+++ VS2019_获取文件类型/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿
+namespace VS2019_获取文件类型
+{
+    partial class FileView_form
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Fileview_lstview = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // Fileview_lstview
+            // 
+            this.Fileview_lstview.AllowDrop = true;
+            this.Fileview_lstview.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Fileview_lstview.HideSelection = false;
+            this.Fileview_lstview.Location = new System.Drawing.Point(0, 0);
+            this.Fileview_lstview.Name = "Fileview_lstview";
+            this.Fileview_lstview.Scrollable = false;
+            this.Fileview_lstview.Size = new System.Drawing.Size(334, 161);
+            this.Fileview_lstview.TabIndex = 0;
+            this.Fileview_lstview.UseCompatibleStateImageBehavior = false;
+            this.Fileview_lstview.DragEnter += new System.Windows.Forms.DragEventHandler(this.Fileview_lstview_DragEnter);
+            // 
+            // FileView_form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 161);
+            this.Controls.Add(this.Fileview_lstview);
+            this.Name = "FileView_form";
+            this.Text = "VS2019_获取文件类型";
+            this.Shown += new System.EventHandler(this.FileView_form_Shown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView Fileview_lstview;
+    }
+}
+
---
+++ VS2019_获取文件类型/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VS2019_获取文件类型
+{
+    public partial class FileView_form : Form
+    {
+        public FileView_form()
+        {
+            InitializeComponent();
+        }
+
+        private void Fileview_lstview_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;//拖放操作：复制
+            String[] str_Drop = (String[])e.Data.GetData(DataFormats.FileDrop, true);//大写String，获取：检索与指定的数据格式关联的数据格式。
+            Data_list(Fileview_lstview, str_Drop[0]);//添加数据项至listview
+        }
+
+        public void Data_list(ListView fileview_lstview, string v)
+        {
+            string filetype = "";
+            if (v.LastIndexOf(".") != -1)//能够检索到"."则：
+            {
+                filetype = v.Substring(v.LastIndexOf(".") + 1);//获取文件后缀名
+            }
+            ListViewItem listViewItem = new ListViewItem(v);//新建listview数据项（行），添加进listview
+            listViewItem.SubItems.Add(filetype);
+            fileview_lstview.Items.Add(listViewItem);
+        }
+
+        private void FileView_form_Shown(object sender, EventArgs e)//程序初次显现样式
+        {
+            //设置listview显示样式
+            Fileview_lstview.GridLines = true;
+            Fileview_lstview.View = View.Details;
+            Fileview_lstview.FullRowSelect = true;
+            Fileview_lstview.HeaderStyle = ColumnHeaderStyle.Nonclickable;
+
+            //添加列
+            Fileview_lstview.Columns.Add("文件名", Fileview_lstview.Width - 120, HorizontalAlignment.Center);
+            Fileview_lstview.Columns.Add("文件类型", 120, HorizontalAlignment.Center);
+        }
+    }
+}
