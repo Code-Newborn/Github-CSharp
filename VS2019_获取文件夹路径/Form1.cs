4fb4e6119fff0f4d0e9f42ef48364354e6f54665
CZ <1253709179@qq.com>
2020-12-08 10:55:13 +0800

    VS2019_获取文件夹路径

---
+++ VS2019_获取文件夹路径/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VS2019_获取文件夹路径
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+            String[] str_Drop = (String[])e.Data.GetData(DataFormats.FileDrop, true);
+            MessageBox.Show(str_Drop[0]);
+        }
+    }
+}
